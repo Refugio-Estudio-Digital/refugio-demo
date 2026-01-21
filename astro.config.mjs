6afdfb7480a57abd7af9a3bf929ba3dd40187b89
TobysNap <naptobys17@gmail.com>
2026-01-21 18:00:48 -0300

    Initial commit v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.fontsource(),
+				name: 'Montserrat',
+				cssVariable: '--font-montserrat',
+			},
+			{
+				provider: fontProviders.fontsource(),
+				name: 'Lora',
+				cssVariable: '--font-lora',
+			},
+		],
+	},
+});
